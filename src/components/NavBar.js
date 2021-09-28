10d7974a6b8b2e091fdbf48d2686df06985ca236
sguia95 <85173512+sguia95@users.noreply.github.com>
2021-09-28 17:33:36 -0300

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+function App() {
+    return <>
+             // Desarrolla tu implementación de un navbar dentro del componente NavBar.js
+             <NavBar />
+             <h2>Inicio</h2>
+             <h2>Consolas</h2>
+             <h2>Juegos</h2>
+             <h2>Contacto</h2>
+     </>;
+}
