6297560dfc6d2004aa4bc1e0f6c462d52ac7ed50
sguia95 <85173512+sguia95@users.noreply.github.com>
2021-09-28 21:08:27 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import NavBar from './components' ;
+import NavBar from './components/NavBar' ;
+
 
 function App() {
   return (
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className ="display-1">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+<NavBar/>
+</div>
   );
 }
 
 export default App;
+
