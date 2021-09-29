7e27ef6082fc109738f81b4ebdfe5c7b529a0dbd
sguia95 <85173512+sguia95@users.noreply.github.com>
2021-09-28 21:07:21 -0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,14 @@
-function App() {
-    return <>
+function NavBar() {
+    return (
          
-             <NavBar />
-             <a> <h2>Inicio</h2> </a>
-             <a><h2>Consolas</h2></a>
-             <a><h2>Juegos</h2></a>
-             <a> <h2>Contacto</h2></a>
-     </>;
+       <div>
+           <h1> Power-Up </h1>
+            <a href=""> <h2>Inicio</h2> </a>
+             <a  href =""><h2>Consolas</h2></a>
+             <a href =""><h2>Juegos</h2></a>
+             <a href =""> <h2>Contacto</h2></a>
+             </div>
+    )
 }
+
+export default NavBar;
